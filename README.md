c78ff3df95500c792b12d7d72ab8b6110ed6ef8a
David Bazile <davidbazile@gmail.com>
2016-04-04 20:12:34 -0400

    Configure build

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
-# new-webapp
+# tinytracker
+
+An over-engineered util that I use to keep track of my hours to make timekeeping reconciliation suck a little less.
+
+I suppose I _could_ just use pen and paper...
+
+http://tinytracker.andsoitcontinues.com/
+
 
 # Running
 
 ```
-$ npm install -g webpack webpack-dev-server
-$ npm install babel-loader
-$ webpack-dev-server
+$ npm install
+$ npm run watch
 ```
+
 
 # Building
 
-lolwut
+```
+$ npm run build
+```
+
 
 # Testing
 
 ```
-$ mocha
+$ npm run test
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 'use strict';
 
+const webpack = require('webpack');
 const path = require('path');
+const package_ = require('./package.json');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  context: path.join(__dirname, 'src'),
+  context: path.join(__dirname, 'app'),
   entry: './index.js',
   devtool: 'source-map',
 
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/src/'
+    publicPath: '/app/'
   },
 
   module: {
@@ -20,8 +23,22 @@ module.exports = {
     loaders: [
       {test: /\.jsx?$/, loader: 'babel', exclude: /node_modules/},
       {test: /\.css$/, loader: 'style!css'},
-      {test: /\.less$/, loader: 'style!css!less'},
+      {test: /\.less$/, loader: 'style!css?modules&localIdentName=[name]__[local]!less'},
       {test: /\.(png|jpg|gif)$/, loader: 'file'}
     ]
-  }
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: `${package_.name} v${package_.version}`,
+      favicon: path.join(__dirname, 'app/images/favicon.png')
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
+
+if (process.env.IS_PROD_BUILD) {
+  const plugins = module.exports.plugins;
+  plugins.push(new webpack.DefinePlugin({'process.env.NODE_ENV': '"production"'}));
+  plugins.push(new webpack.UglifyJsPlugin());
+}
