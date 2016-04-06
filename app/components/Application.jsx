5765bac80da4b690245be72a06d2eacc52cb9fb3
David Bazile <davidbazile@gmail.com>
2016-04-06 02:08:21 -0400

    Persist state in local storage

--- app/components/Application.jsx
+++ app/components/Application.jsx
@@ -3,25 +3,19 @@ import styles from './Application.less';
 import Week from './Week';
 import HoursRequired from './HoursRequired';
 import HoursRemaining from './HoursRemaining';
-import {aggregate, DAYS} from '../utils/time';
-
-const [, MON, TUE, WED, THU, FRI] = DAYS;
+import {aggregate} from '../utils/time';
+import {deserialize, serialize} from '../utils/store';
 
 export default class Application extends Component {
   constructor() {
     super();
     this._daysChanged = this._daysChanged.bind(this);
     this._requiredHoursChanged = this._requiredHoursChanged.bind(this);
-    this.state = {
-      days: [
-        {name: MON, times: [[], [], [], [], []]},
-        {name: TUE, times: [['09:00', '17:00'], [], [], [], []]},
-        {name: WED, times: [[], [], [], [], []]},
-        {name: THU, times: [[], [], [], [], ['16:00', '19:00']]},
-        {name: FRI, times: [[], [], [], [], []]}
-      ],
-      requiredHours: 40
-    };
+    this.state = deserialize();
+  }
+
+  componentDidUpdate() {
+    serialize(this.state);
   }
 
   render() {
